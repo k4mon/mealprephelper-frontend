fcc78b9b4eedd5dede6b3a096ebb84624b78f5e8
Maciej Deregowski <maciej.deregowski@gmail.com>
2020-05-04 00:42:34 +0200

    fixed authheader

--- src/services/authHeaders.js
+++ src/services/authHeaders.js
@@ -1,5 +1,5 @@
 const getAuthHeaders = () => {
-  return localStorage.getItem("currentUser") ? { Authorization: `Bearer ${localStorage.getItem("currentUser").access_token}`} : {}
+  return localStorage.getItem("currentUser") ? { Authorization: `Bearer ${JSON.parse(localStorage.getItem("currentUser")).data.access_token}`} : {}
 }
 
 export default getAuthHeaders;
